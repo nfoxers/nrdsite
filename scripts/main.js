5c009ef6f39f3f71a8a756273aa6de604ba9dd9e
wkokplauo666 <narada.fox7pc@gmail.com>
2025-04-19 23:15:34 +0700

    Release

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,3 @@
-function change_ip(element) {
-  fetch("https://api.ipify.org?format=json")
-    .then(response => response.json())
-    .then(data => {
-      element.innerHTML = data.ip;
-    })
-    .catch(error => {
-      element.innerHTML = "127.0.0.1 (E)"
-      console.error("Error fetching IP address: ", error);
-    });
-}
 
 function change_hname(addr, port) {
   addr.innerHTML = window.location.hostname;
@@ -32,6 +21,57 @@ function change_stat(uptime, tvisit, uvisit) {
   });
 }
 
+async function get_json(url) {
+  try {
+    const res = await fetch(url);
+
+    if(!res.ok) {
+      throw new Error(`HTTP error, status: ${res.status}`);
+    }
+
+    const data = await res.json();
+    return data;
+  } catch(e) {
+    console.error(e);
+    return null;
+  }
+}
+
+async function post_json(url, data) {
+  try {
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data, null, 2),
+    });
+
+    if(!res.ok) {
+      throw new Error(`HTTP post error, status: ${res.status}`);
+    }
+
+    const result = await res.json();
+    return result;
+  } catch(e) {
+    console.error(e);
+    return null;
+  }
+}
+
+function get_date() {
+  const now = new Date();
+  const formatted = now.toLocaleString([], {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+  });
+  return formatted;
+}
+
 function main() {
   const ipaddr = document.getElementById("ipaddr");
   const haddr = document.getElementById("hostname");
@@ -40,9 +80,69 @@ function main() {
   const tvisit = document.getElementById("visit_y");
   const uvisit = document.getElementById("visit_n");
   
-  change_ip(ipaddr);
   change_hname(haddr, port);
   change_stat(uptime, tvisit, uvisit);
+
+  fetch("https://api.ipify.org?format=json")
+  .then(response => response.json())
+  .then(data => {
+    ipaddr.innerHTML = data.ip;
+    let dataj = {
+      ip: data.ip,
+      time: get_date(),
+    }
+
+    post_json("/api/usr_visit", dataj);
+    change_hname(haddr, port);
+    change_stat(uptime, tvisit, uvisit);
+  })
+  .catch(error => {
+    console.error("Error fetching IP address: ", error);
+    ipaddr.innerHTML = "127.0.0.1 (E)";
+  });
+
+
+  new TypeIt("#name", {
+    speed: 100,
+    waitUntilVisible: true,
+  })
+    .type("Narsdq", { delay: 750 })
+    .delete(3)
+    .type("ada")
+    .move(null, { to: "END" })
+    .type(" <span class='orange'>Fox</span>.")
+    .pause(5000)
+    .delete(11)
+    .type("a <span class='orange'>fox</span>.")
+    .go();
+
+  document.getElementById("mainimage").addEventListener("click", () => {
+    document.getElementById("kliks").innerHTML++;
+  });
+
+  const form = document.getElementById("form");
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const formdata = new FormData(form);
+
+    fetch("https://api.ipify.org?format=json")
+    .then(response => response.json())
+    .then(data => {
+      let msg = {
+        name: formdata.get("name"),
+        ip: data.ip,
+        message: formdata.get("text"),
+        time: get_date(),
+      }
+
+      post_json("/api/usr_msg", msg);
+    })
+    .catch(error => {
+      console.error("Error fetching IP address: ", error);
+    });
+  });
+
 }
 
 main();
--- server.js
+++ server.js
@@ -4,21 +4,12 @@ const https = require("https");
 const http = require("http");
 const path = require("path")
 const app = express();
-const port = 8080;
 
-app.use(express.json());
+/* configurations & boilerplate variables */
 
-function readjson(pth) {
-  p = path.join(__dirname, pth);
-  if(!fs.existsSync(p)) return {};
-  const data = fs.readFileSync(p, 'utf8');
-  return JSON.parse(data || "{}");
-}
-
-function writejson(pth, data) {
-  p = path.join(__dirname, pth);
-  fs.writeFileSync(p, JSON.stringify(data, null, 2), 'utf8');
-}
+let http_port = 80;
+let https_port = 443;
+let https_enable = true;
 
 let system = {
   uptime: 0,
@@ -27,11 +18,54 @@ let system = {
   node: false
 }
 
+let visits = {
+  users: {
+    "127.0.0.1": {
+      first_visited: "...",
+      last_visited: "...",
+      visits: 0,
+    },
+  },
+}
+
+let messages = {
+  name: {
+    msg: "...",
+    time: "...",
+    ip: "..."
+  }
+}
+
+/* initialization */
+
+app.use(express.json());
+
+function readjson(pth) {
+  p = path.join(__dirname, pth);
+  if(!fs.existsSync(p)) return {};
+  try {
+    const data = fs.readFileSync(p, 'utf8');
+    return JSON.parse(data || "{}");    
+  } catch(e) {
+    console.error(`error reading json file ${pth}: ${e}`);
+  }
+}
+
+function writejson(pth, data) {
+  p = path.join(__dirname, pth);
+  fs.writeFileSync(p, JSON.stringify(data, null, 2), 'utf8');
+}
+
 system = readjson("data/system.json");
+visits = readjson("data/visits.json");
+messages = readjson("data/messages.json");
+
 if(system.node == false) {
   system.node = true;
   writejson("data/system.json", system);
 }
+
+/* route handlers */
 
 app.get("/{*splat.html}", (req, res, next) => {
   console.log(`${req.ip} opened ${req.path}`);
@@ -39,43 +73,97 @@ app.get("/{*splat.html}", (req, res, next) => {
   next();
 });
 
-app.get("/", (req, res, next) => {
+app.post("/api/usr_visit", (req, res) => {
+  const obj = req.body;
+
   system = readjson("data/system.json");
   system.uvisits++;
   system.tvisits++;
   system.uptime = (process.uptime()/60).toFixed(2) ;
   writejson("data/system.json", system);
   console.log(system);
-  next();
+
+  visits = readjson("data/visits.json");
+
+  let found = 0;
+  for(let key in visits.users) {
+    if(key == obj.ip) {
+      found = 1;
+      visits.users[key].last_visited = obj.time;
+      visits.users[key].visits++;
+    }
+  }
+  if(!found) {
+    visits.users[obj.ip] = {
+      first_visited: obj.time,
+      last_visited: obj.time,
+      visits: 1
+    }
+    console.log(`${obj.ip} is a new user`);
+  }
+  writejson("data/visits.json", visits);
+  
+  res.status(200).json({ success: true });
+})
+
+app.post("/api/usr_msg", (req, res) => {
+  const obj = req.body;
+
+  messages = readjson("data/messages.json");
+  
+  messages[obj.name] = {};
+  messages[obj.name].msg = obj.message;
+  messages[obj.name].time = obj.time;
+  messages[obj.name].ip = obj.ip;
+  
+  writejson("data/messages.json", messages);
+
+  console.log(messages);
+  res.status(200).json({ success: true });
 })
 
 app.use(express.static("."));
 
-app.get("/*splat", (req, res, next) => {
-  console.log(`${req.ip} opened a page that does not exist`);
+app.get("/*splat", (req, res) => {
+  console.log(`${req.ip} opened a page that does not exist (${req.url})`);
   res.status(404).sendFile(path.join(__dirname, "pages/notfound.html"));
 });
 
-http.createServer(app).listen(80, () => {
-  console.log("server is running on port 80 HTTP");
+/* server */
+
+if(process.getuid && process.getuid() != 0) {
+  console.log("not ran by root, ports will be modified");
+  http_port += 8000;
+  https_port += 8000;
+}
+
+http.createServer(app).listen(http_port, () => {
+  console.log(`server is running on port ${http_port} HTTP`);
 }).on("error", (err) => {
   console.error(err);
 });
 
-const sslopt = {
-  key: fs.readFileSync(path.join(__dirname, "env/server.key")),
-  cert: fs.readFileSync(path.join(__dirname, "env/server.cert"))
-};
+if(https_enable) {
+  if(!fs.existsSync("env/server.key") || !fs.existsSync("env/server.cert")) {
+    console.error("SLS credentials not found, please provide your own and store it at env/server.key and env/server.cert");
+  } else {
+      const sslopt = {
+      key: fs.readFileSync(path.join(__dirname, "env/server.key")),
+      cert: fs.readFileSync(path.join(__dirname, "env/server.cert"))
+    };
 
-https.createServer(sslopt, app).listen(443, () => {
-  console.log("server is running on port 443 HTTPS");
-}).on("error", (err) => {
-  console.error(err);
-});
+    https.createServer(sslopt, app).listen(https_port, () => {
+      console.log(`server is running on port ${https_port} HTTPS`);
+    }).on("error", (err) => {
+      console.error(err);
+    });
+  }
+}
 
+/* cleanup */
 
 process.on("SIGINT", () => {
-  console.log("exiting...");
+  console.log("\nexiting...");
 
   system.uptime = 0;
   system.uvisits = 0;
